ab37228a7331c531a82e05b73e5c87e559b5c588
Abdisalam Ali <abdisharifali@yahoo.com>
2025-05-14 12:40:53 -0500

    trying #2 fixing article rendering

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,26 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-9fa609ab4a11bbbc.js","static/chunks/4bd1b696-ab28d8fb5bb10686.js","static/chunks/684-8172b5f9dec28286.js","static/chunks/main-app-8ab7b4a4534b9215.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-9fa609ab4a11bbbc.js","static/chunks/framework-b326bfe0905a39d9.js","static/chunks/main-5b0522cc452e73dd.js","static/chunks/pages/_app-da15c11dea942c36.js"],"/_error":["static/chunks/webpack-9fa609ab4a11bbbc.js","static/chunks/framework-b326bfe0905a39d9.js","static/chunks/main-5b0522cc452e73dd.js","static/chunks/pages/_error-cc3f077a18ea1793.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [
+    "static/chunks/node_modules_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_fd44f5a4._.js",
+    "static/chunks/node_modules_next_dist_compiled_2ce9398a._.js",
+    "static/chunks/node_modules_next_dist_client_8f19e6fb._.js",
+    "static/chunks/node_modules_next_dist_2ecbf5fa._.js",
+    "static/chunks/node_modules_@swc_helpers_cjs_00636ac3._.js",
+    "static/chunks/_e69f0d32._.js",
+    "static/chunks/_93808211._.js"
+  ],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
---
+++ .next/static/chunks/src_app_articles_page_tsx_ce421977._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_3c6c868f._.js",
+  "static/chunks/node_modules_@firebase_auth_dist_esm2017_7bc5098a._.js",
+  "static/chunks/node_modules_@firebase_firestore_dist_index_esm2017_c2fcaa2e.js",
+  "static/chunks/node_modules_micromark-core-commonmark_dev_lib_36a4b45d._.js",
+  "static/chunks/node_modules_f8be884e._.js",
+  "static/chunks/src_styles_a357f5c5._.css"
+],
+    source: "dynamic"
+});
